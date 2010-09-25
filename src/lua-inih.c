a3f12924b104e6fdc5aa6a03728c14cca4b0aaa5
Alexander Gladysh <agladysh@gmail.com>
2010-09-25 16:28:24 +0400

    lua-inih: implemented inih.parse()

--- src/lua-inih.c
+++ src/lua-inih.c
@@ -19,16 +19,135 @@ extern "C" {
 
 #include "ini.h"
 
+static int callback(
+    void * user,
+    const char * section,
+    const char * name,
+    const char * value
+  )
+{
+  lua_State * L = (lua_State *) user;
+  int base = lua_gettop(L);
+
+  /* TODO: Hack. Fix inih to stop at first error and remove this check */
+  if (!lua_isfunction(L, -1))
+  {
+    /* Assuming we have an error message on stack */
+    return 0;
+  }
+
+  lua_pushvalue(L, base); /* the function itself */
+  lua_pushstring(L, section);
+  lua_pushstring(L, name);
+  lua_pushstring(L, value);
+
+  if (lua_pcall(L, 3, 2, 0) != 0)
+  {
+    /* TODO: provide better error reporting */
+    const char * error_message = lua_tostring(L, -1);
+
+    lua_pop(L, 1);
+
+    lua_pushnil(L);
+    lua_pushfstring(
+        L, "callback error: %s",
+        (error_message != NULL)
+          ? error_message
+          : "(non-string error message)"
+      );
+
+    return 0; /* We've encountered an error */
+  }
+
+  if (!lua_toboolean(L, base + 1))
+  {
+    const char * error_message = lua_tostring(L, base + 2);
+    lua_pop(L, 2);
+
+    lua_pushnil(L);
+    lua_pushstring(
+        L,
+        (error_message != NULL)
+          ? error_message
+          : "(unintelligible error from callback)"
+      );
+
+    return 0; /* We've encountered an error */
+  }
+
+  lua_pop(L, 2); /* Pop results */
+
+  return 1; /* Continue parsing. */
+}
+
 /*
 * inih.parse(filename : string, callback : function) : true / nil, error_message
 */
 static int l_parse(lua_State * L)
 {
-  size_t len = 0;
-  const char * filename = luaL_checklstring(L, 1, &len);
+  int base = lua_gettop(L);
+  int error_line = 0;
+
+  const char * filename = luaL_checkstring(L, 1);
   luaL_checktype(L, 2, LUA_TFUNCTION);
 
-  return luaL_error(L, "TODO: Implement");
+  error_line = ini_parse(filename, callback, L);
+  if (error_line != 0)
+  {
+    const char * error_message = NULL;
+
+    int new_base = lua_gettop(L);
+    if (new_base == base)
+    {
+      lua_pushnil(L);
+      lua_pushstring(
+          L,
+          (error_line == -1)
+            ? "failed to open file"
+            : "bad file format" /* TODO: Pick a better error message */
+        );
+    }
+    else if (new_base != base + 2)
+    {
+      lua_settop(L, base);
+      return luaL_error(
+          L, "lua-inih: bad implementation, garbage on stack on error"
+        );
+    }
+
+    error_message = lua_tostring(L, -1);
+    lua_pop(L, 1);
+
+    /* TODO: Trim filename */
+
+    if (error_line == -1)
+    {
+      lua_pushfstring(
+          L, "%s: %s",
+          filename,
+          (error_message != NULL)
+            ? error_message
+            : "unintelligible error message"
+        );
+    }
+    else
+    {
+      lua_pushfstring(
+          L, "%s:%d: %s",
+          filename,
+          error_line,
+          (error_message != NULL)
+            ? error_message
+            : "unintelligible error message"
+        );
+    }
+
+    return 2; /* nil and error message are already on stack */
+  }
+
+  lua_pushboolean(L, 1);
+
+  return 1;
 }
 
 /* luabins Lua module API */
